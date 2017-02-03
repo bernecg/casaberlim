6c9b1fadee676249653622a20f3868b386dbaf1b
Bernard Caye <bernard.caye@sap.com>
2017-02-03 16:23:44 -0200

    Continuing UI5 tutorial - current step 11

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
-   return Controller.extend("sap.ui.casaberlim.controller.App", {
-      onShowHello : function () {
-         // show a native JavaScript alert
-         alert("Hello World");
-      }
-   });
+    return Controller.extend("bcg.casaberlim.controller.App", {
+        onShowHello : function () {
+            // read msg from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            // show message
+            MessageToast.show(sMsg);
+        }
+    });
 });
